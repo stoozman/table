766a4a96b9032d5aea51a3197d73531f35112626
stoozman <stoozman@yandex.ru>
2025-05-29 17:59:55 +0300

    test: несущественный комментарий для проверки git-коммита

--- src/SignDocumentUploadPage.js
+++ src/SignDocumentUploadPage.js
@@ -1,3 +1,4 @@
+// Несущественный комментарий для теста git-коммита
 import React, { useState, useEffect } from 'react';
 import { saveDocumentToDropbox, getDropboxShareableLink, deleteDocumentFromDropbox, listDropboxFiles, createDropboxFolder } from './utils/documentGenerator';
 import './SignDocumentPage.css';
